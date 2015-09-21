379d08aee3c620b238b509cb4ea8e9895a6cfa5b
emhedenma <emhedenma@mycompany.com>
2015-09-21 11:56:41 +0000

    Reduced the number of Union-VMS features. Version is now 0.0.3-SNAPSHOT. @review Marcus, Gustav
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/usm4uvms@4648 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- src/main/java/eu/europa/ec/fisheries/uvms/rest/security/UnionVMSFeature.java
+++ src/main/java/eu/europa/ec/fisheries/uvms/rest/security/UnionVMSFeature.java
@@ -4,31 +4,21 @@ package eu.europa.ec.fisheries.uvms.rest.security;
  * Defines all known features
  */
 public enum UnionVMSFeature {
-    /** View Vessels */
-    getVesselList,
-    getVesselById,
-    /** Create vessels */
-    createVessel,
-    /** Update vessels */
-    updateVessel,
-    /** Access vessel history */
-    vesselHistory,
-    /** View vessel groups */
-    viewVesselGroups,
-    /** Manage vessel groups */
-    manageVesselGroups,
-    /** Accessing vessel configuration */
-    vesselConfig,
 
-    /** Viewing mobile terminals */
-    viewMobileTerminals,
-    /** Managing mobile terminals */
-    manageMobileTerminals,
-    /** Mobile terminal plugins */
-    mobileTerminalPlugins,
-    /** Accessing mobile terminal configuration */
-    mobileTerminalConfig,
+	/** Viewing both vessels and mobile terminals */
+	viewVesselsAndMobileTerminals,
+	/** Managing mobile terminals */
+	manageMobileTerminals,
+	/** Managing vessels */
+	manageVessels,
 
+	/** Viewing mobile terminal polls */
+	viewMobileTerminalPolls,
+	/** Managing mobile terminal polls */
+	managePolls,
+
+	mobileTerminalPlugins,
+	
     /** Viewing movements */
     viewMovements,
     /** Managing movements */
@@ -58,15 +48,6 @@ public enum UnionVMSFeature {
     manageTransmissionStatus,
     /** Accessing exchange log configuration */
     exchangeConfig,
-
-    /** Viewing polling logs */
-    viewPollingLogs,
-    /** Managing program polls */
-    manageProgramPolls,
-    /** Creating new polls */
-    managePolls,
-    /** Viewing pollable channels */
-    viewPollableChannels,
 
     /** Viewing audit logs */
     viewAuditLogs,
