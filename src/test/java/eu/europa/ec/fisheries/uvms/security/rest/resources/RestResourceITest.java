690ff4eaa328be48598c396e15f797b0e54e3855
Georgi Georgiev <georgi.georgiev@ordina.be>
2015-09-29 16:21:33 +0000

    Minor improvements
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/usm4uvms@5059 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- src/test/java/eu/europa/ec/fisheries/uvms/security/rest/resources/RestResourceITest.java
+++ src/test/java/eu/europa/ec/fisheries/uvms/security/rest/resources/RestResourceITest.java
@@ -7,11 +7,16 @@ import eu.europa.ec.fisheries.uvms.rest.security.AuthorizationFilter;
 import eu.europa.ec.fisheries.uvms.rest.security.JwtTokenHandler;
 import eu.europa.ec.fisheries.uvms.security.rest.util.ArquillianTest;
 import eu.europa.ec.mare.usm.information.domain.*;
+import eu.europa.ec.mare.usm.information.domain.Context;
+import eu.europa.ec.mare.usm.information.domain.Feature;
 import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.extension.rest.client.ArquillianResteasyResource;
 import org.jboss.arquillian.extension.rest.client.Header;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.resteasy.client.jaxrs.ProxyBuilder;
+import org.jboss.resteasy.client.jaxrs.ResteasyClient;
+import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
 import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
 import org.junit.After;
 import org.junit.Before;
@@ -22,13 +27,14 @@ import javax.ejb.EJB;
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.client.Entity;
-import javax.ws.rs.core.HttpHeaders;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.core.*;
 import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Method;
+import java.net.URI;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.Set;
+import java.util.*;
 
 import static org.junit.Assert.assertEquals;
 
@@ -37,12 +43,60 @@ import static org.junit.Assert.assertEquals;
 public class RestResourceITest extends ArquillianTest {
 	
     @ArquillianResource
-    URL contextPath;
-
+    private URL contextPath;
 
 	@Before
 	public void setUp() throws Exception {
+		UserContext newUserCtx = new UserContext();
 
+		Feature newFeature = new Feature();
+		newFeature.setApplicationName("TEST_APP");
+		newFeature.setFeatureName("APP1_TEST_FEATURE");
+		Set<Feature> features = new HashSet<>();
+		features.add(newFeature);
+
+		Role newRole = new Role();
+		newRole.setRoleName("TEST_ROLE");
+		newRole.setFeatures(features);
+
+		Preferences newPrefs = new Preferences();
+//		Preference pref = new Preference();
+//		pref.setApplicationName(newFeature.getApplicationName());
+//		pref.setOptionName("TEST_USER_PREFERENCE");
+//		pref.setOptionValue("CUSTOM_USER_VALUE");
+		Set<Preference> prefSet = new HashSet<>();
+//		prefSet.add(pref);
+		newPrefs.setPreferences(prefSet);
+
+		Scope newScope = new Scope();
+		Calendar todayCal = Calendar.getInstance();
+		newScope.setActiveFrom(todayCal.getTime());
+		todayCal.add(Calendar.YEAR, 3);
+		newScope.setActiveTo(todayCal.getTime());
+		newScope.setScopeName("TEST_SCOPE");
+
+		Context newCtx = new Context();
+		newCtx.setRole(newRole);
+		newCtx.setPreferences(newPrefs);
+		newCtx.setScope(newScope);
+
+		ContextSet ctxSet = new ContextSet();
+		Set<Context> ctxs = new HashSet<>();
+		ctxs.add(newCtx);
+		ctxSet.setContexts(ctxs);
+
+		newUserCtx.setApplicationName(newFeature.getApplicationName());
+		newUserCtx.setContextSet(ctxSet);
+		newUserCtx.setUserName("TEST_USER");
+//		InputStream in = getClass().getResourceAsStream("config.properties");
+//		Properties props = new Properties();
+//		props.load(in);
+
+		ResteasyWebTarget usmWebTarget = new ResteasyClientBuilder().build().target("http://localhost:8080/usm-information/rest");
+		Response response = usmWebTarget.path("/userContext").request(MediaType.APPLICATION_JSON_TYPE)
+				.header(HttpHeaders.AUTHORIZATION, new JwtTokenHandler().createToken("TEST_USER")).put(Entity.json(newUserCtx));
+
+		assert response.getStatus() == HttpServletResponse.SC_OK;
 	}
 
 	@After
