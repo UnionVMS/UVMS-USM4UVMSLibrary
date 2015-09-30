c91edf9bee1ca012d45684b170990cf55650f6c2
Georgi Georgiev <georgi.georgiev@ordina.be>
2015-09-30 16:29:25 +0000

    improving the ModuleInitializerBean
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/usm4uvms@5094 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

---
+++ src/test/java/eu/europa/ec/fisheries/uvms/init/TestModuleInitializerBean.java
@@ -0,0 +1,36 @@
+package eu.europa.ec.fisheries.uvms.init;
+
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * Created by georgige on 9/30/2015.
+ */
+@Singleton
+@Startup
+public class TestModuleInitializerBean extends AbstractModuleInitializerBean {
+
+    public static final String CONFIG_USM_DEPLOYMENT_DESCRIPTOR_XML = "usmDeploymentDescriptor.xml";
+    public static final String PROP_FILE_NAME = "config.properties";
+
+    @Override
+    protected Properties retrieveModuleConfigs() throws IOException {
+        Properties prop = new Properties();
+        InputStream properties = getClass().getClassLoader().getResourceAsStream(PROP_FILE_NAME);
+        if (properties != null) {
+            prop.load(properties);
+            return prop;
+        } else {
+            throw new FileNotFoundException("Property file '" + PROP_FILE_NAME + "' not found in the classpath");
+        }
+    }
+
+    @Override
+    public InputStream getDeploymentDescriptor() {
+        return  getClass().getClassLoader().getResourceAsStream(CONFIG_USM_DEPLOYMENT_DESCRIPTOR_XML);
+    }
+}
