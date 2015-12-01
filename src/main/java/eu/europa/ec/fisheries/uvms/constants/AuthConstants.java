ff1c8fe6bc306f9b9612a7298761dbe2fb162aca
Georgi Georgiev <georgi.georgiev@ordina.be>
2015-12-01 14:46:04 +0000

    small formatting improvement
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/usm4uvms@7151 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- src/main/java/eu/europa/ec/fisheries/uvms/constants/AuthConstants.java
+++ src/main/java/eu/europa/ec/fisheries/uvms/constants/AuthConstants.java
@@ -6,11 +6,10 @@ import javax.ws.rs.core.HttpHeaders;
  * Created by georgige on 10/2/2015.
  */
 public interface AuthConstants {
-    public static final String HTTP_HEADER_ROLE_NAME = "roleName";
-
+    String HTTP_HEADER_ROLE_NAME = "roleName";
     String HTTP_HEADER_SCOPE_NAME = "scopeName";
-
     String HTTP_HEADER_AUTHORIZATION = HttpHeaders.AUTHORIZATION;
+
     String JWTCALLBACK = "jwtcallback";
 
     String HTTP_SERVLET_CONTEXT_ATTR_FEATURES = "servletContextUserFeatures";
